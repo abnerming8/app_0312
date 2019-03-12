ef8fec5c3ef99252409f737c7203584716e1554c
AbnerMing <598254259@qq.com>
2019-03-12 15:52:32 +0800

    提交信息

---
+++ app/src/main/java/android/vip/com/app0312_z/MainActivity.java
@@ -0,0 +1,13 @@
+package android.vip.com.app0312_z;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
